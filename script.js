b7f3447a67423ebd9d2b32b99670a6c2cfa15eba
DevLJW <jaewookv123@gmail.com>
2023-02-01 16:24:19 +0900

    list and button add

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
+const todoinput = document.querySelector("#todo-input");
+
+const createTodo = function () {
+  const todolist = document.querySelector("#todo-list");
+
+  const newLi = document.createElement("li"); //새로운 li태그
+  const newspan = document.createElement("span"); //새로운 span태그
+  const newBtn = document.createElement("button");
+
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete"); //li태그에 class 이름 추가
+  });
+
+  newspan.textContent = todoinput.value;
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newspan);
+  todolist.appendChild(newLi); //기존에 있던 todo-list아이디에 값 붙히기
+  todoinput.value = " ";
+};
 const keyCodeCheck = function () {
-  const todoinput = document.querySelector("#todo-input");
-
   if (window.event.keyCode === 13 && todoinput.value !== "") {
-    const todolist = document.querySelector("#todo-list");
-
-    const newLi = document.createElement("li"); //새로운 li태그
-    const newspan = document.createElement("span"); //새로운 span태그
-
-    newspan.textContent = todoinput.value;
-    newLi.appendChild(newspan);
-    todolist.appendChild(newLi); //기존에 있던 todo-list아이디에 값 붙히기
-    todoinput.value = " ";
+    createTodo();
   }
 };
